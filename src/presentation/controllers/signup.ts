7b04c8884aa59944c5a46c71494e92524c44452e
Diego-Nascimento <diegostealth3@gmail.com>
2021-01-25 17:36:58 -0300

    refactor: move replicated code to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,20 +1,15 @@
+import { badRequest } from './../Helpers/http-helpers'
 import { MissingParamError } from './../Errors/MissingParamError'
 import { HttpRequest } from './../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): any {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
+      return badRequest(new MissingParamError('email'))
     }
   }
 }
